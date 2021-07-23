32630bbc00b503303e7bd4dd43699f76f73bf9b7
RATHIKA <rathisekar22@gmail.com>
2021-07-23 19:31:41 +0530

    first commit

---
+++ src/Pages/Products.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Products = () => {
+    return (
+        <div class="w-9/12">
+        <div class="p-4 text-gray-500">
+            This page for items and product
+        </div>
+    </div>
+    )
+}
+
+export default Products
