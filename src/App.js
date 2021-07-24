22a10778d7e899dc4c7f97717798e01532427a79
RATHIKA <rathisekar22@gmail.com>
2021-07-24 08:04:21 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+import Sidebar from '../src/Components/Sidebar';
+import { BrowserRouter as Router, Switch,
+Route} from 'react-router-dom';
+
+function App() {
+  return (
+        < >
+    <Router>
+    <Sidebar />
+<Switch>
+  <Route path='/' />
+</Switch>
+    </Router>
+      </>
+  );
+}
+
+export default App;
